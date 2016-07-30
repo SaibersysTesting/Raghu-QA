de7d830ec3d22b402bbaa438a2cb768595ef76f4
arun teja <training.saibersys9@gmail.com>
2016-07-30 15:57:25 -0500

    Add files via upload
    selenium java with customized xpath and handling the different locators.

--- Locater1.java
+++ Locater1.java
@@ -1,4 +1,4 @@
-package chromedriver;
+package locater;
 
 import org.openqa.selenium.By;
 
@@ -7,22 +7,19 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Locater1 {
 
-	
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    WebDriver driver = new FirefoxDriver();
-    driver.get("http://www.amazon.com");
-    driver.findElement(By.xpath(".//*[@id='nav-link-yourAccount']")).click();
-    driver.findElement (By.xpath(".//*[@id='nav-flyout-ya-signin']/a/span")).click();
-    driver.findElement(By.id(".//*[@id='ap_email']")).sendKeys("vickyraghu4@gmail.com");
-    driver.findElement(By.className(".//*[@id='ap_password']")).sendKeys("vickyraghu1989");
-    driver.findElement(By.xpath(".//*[@id='signInSubmit']")).click();
-   
-    
-    /*driver.get("http://www.udemy.com");
-    driver.findElement(By.xpath(".//*[@id='udemy']/div[2]/div[1]/div[2]/div[2]/form/button")).click();*/
-    
-   }
+
+		WebDriver driver = new FirefoxDriver();
+		/*driver.get("http://www.msn.com");
+		driver.findElement(By.xpath(".//*[@id='sb_form_go']")).click();*/
+		driver.get("http://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("raghu1234");
+		driver.findElement(By.name("pass")).sendKeys("12345");
+		driver.findElement(By.xpath(".//*[@id='login_form']/table/tbody/tr[3]/td[2]/a")).click();
+		
+		
+		
+	}
 
 }
